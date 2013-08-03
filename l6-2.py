1db8ae7ce0474a818bb34fe2dd835c21013ad076
leon <wpctszz@qq.com>
2013-08-03 13:52:16 +0800

    level6

---
+++ l6-2.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+import zipfile
+dir = "files" + "/"
+file = dir + "channel.zip"
+zip_file = zipfile.ZipFile(file)
+comment =''
+file_name = '90052.txt'
+zc =[]
+while 1:
+	f = open('files/%s' % file_name)
+	next_num = f.read().split()[-1]
+	f.close
+	zc.append(zip_file.getinfo(file_name).comment)
+	if next_num.isdigit():
+		file_name = next_num + '.txt'
+	else:
+		break
+print ''.join(zc)
---
+++ l6.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+dir = "files" + "/"
+file = dir + "90052.txt"
+while 1:
+	file = open(file)
+	letter = file.read()
+	file.close()
+	get_last=''.join(letter.split(" ")[-1:])
+	if get_last.isdigit():
+		file = dir + get_last + '.txt'
+		print file
+	else:
+		print letter
+		break
